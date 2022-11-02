9f2cdda5ee8561fb9b238c043445a6ade8ca9352
Dina <Dinamuktar21@gmail.com>
2022-11-02 15:49:51 +0300

    a

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,16 +2,19 @@ import '../App.css'
 const Login = () => {
   return (
     <div className='loginStyle'>
-        <div style={{marginTop:"50px", marginBottom:"0"}}>
-            <p className='emailStyle'><span>&#9993;</span>Email</p>
-            <div className='box'></div>
-        </div>
-
         <div>
-            <p className='emailStyle'>Password</p>
+            <div className='emailDiv'>
+                <p className='emailStyle'><span>&#9993;</span>Email</p>
+                <div className='box'></div>
+            </div>
+            
+        <div className='passDiv'>
+            <p>Password</p>
             <div className='box'></div>
-        </div>            
+    </div>
 
+        </div>
+        
             <button className='login'>Login</button>
     </div>
   )

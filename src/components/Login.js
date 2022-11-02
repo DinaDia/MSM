421fb4c15e060a2d2ea0b4cbd0fffc941202a992
Dina <Dinamuktar21@gmail.com>
2022-11-02 16:16:01 +0300

    4.15

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,28 @@
-import '../App.css'
+import '../App.css';
+import {Link} from 'react-router-dom';
+
 const Login = () => {
+
+
   return (
     <div className='loginStyle'>
         <div>
             <div className='emailDiv'>
-                <p className='emailStyle'><span>&#9993;</span>Email</p>
+                <p className='loginTextStyle'><span>&#9993;</span>Email</p>
                 <div className='box'></div>
             </div>
             
         <div className='passDiv'>
-            <p>Password</p>
+            <p className="loginTextStyle">Password</p>
             <div className='box'></div>
-    </div>
-
         </div>
-        
-            <button className='login'>Login</button>
+
+    </div>
+    
+    <button className='login'>
+        <Link style={{color:'white', textDecoration:'none'}}
+        >Login</Link>
+    </button>
     </div>
   )
 }

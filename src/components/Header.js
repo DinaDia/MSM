ebb88c6720dd89a13d5a38337e722cda8a41c971
Dina <Dinamuktar21@gmail.com>
2022-11-02 14:49:54 +0300

    initial

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <div className="headerStyle">
+      <img style={{marginLeft:"20px"}} src="https://www.cdt-africa.org/templates/g5_helium/custom/images/Logo/Asset%206logo%20revisedd%20.png" alt="logo"/>
+      <button className="addPatientButton">Add new patient</button>
+      
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import '../App.css'
+const Login = () => {
+  return (
+    <div className='loginStyle'>
+        <div className='emailStyle'>
+            <p><span>&#9993;</span>Email</p>
+            <div className='box'></div>
+
+
+            <div className='login'>Login</div>
+            </div>
+    </div>
+  )
+}
+
+export default Login
